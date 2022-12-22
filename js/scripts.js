693d022fa6e2248b94ca5445bd063721bb37cdd2
Gabr19 <sannagabriele19@gmail.com>
2022-12-22 11:39:56 +0100

    inizio

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 100; i += 1) {
+
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzBuzz');
+    }
+
+    else if(i % 3 == 0) {
+        console.log('fizz');
+    }
+
+    else if(i % 5 == 0) {
+        console.log('buzz');
+    }
+
+    else{
+        console.log('i', i, typeof i)
+    }
+
+}
